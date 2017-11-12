e7aa59342bdce37b11eb68c676538fc04829f42e
Alexis Petrillo <alexispetrillo@hotmail.fr>
2017-11-12 23:40:35 +0100

    Fix PR

--- sources/table/table.c
+++ sources/table/table.c
@@ -1,34 +1,36 @@
 /*
-  Filename: table.c
-
-  Made by: Alexis PETRILLO on 12/11/2017.
-
-**  Description: 
+**  Filename: table.c
+**
+**  Made by: Alexis PETRILLO on 12/11/2017.
+**
+**  Description: Functions for manage tables
 */
 
+#include "../database/database.h"
 #include "table.h"
 
 /**
- * Remove a table
- * @param databaseName, tableName
+ *
+ * @param databaseName
+ * @param tableName
  * @return 0 if success, 1 for error
  */
 int removeTable(const char *databaseName, const char *tableName) {
     char *path;
 
-    path = xmalloc(sizeof(char) * strlen(RESOURCES_DIR) + strlen
-            (tableName), __func__);
+    path = xmalloc(sizeof(char) * (strlen(RESOURCES_DIR) + strlen
+            (tableName) + strlen(tableName) + 5), __func__);
     if (!path)
         return 1;
 
-    strcat(path, RESOURCES_DIR);
+    strcpy(path, RESOURCES_DIR);
     strcat(path, databaseName);
     strcat(path, "/");
     strcat(path, tableName);
     strcat(path, ".yml");
 
     if (remove(path) == -1) {
-        fprintf(stderr, "An error has occured when creating table '%s': "
+        fprintf(stderr, "An error has occured when removing table '%s': "
                 "%s\n", tableName, strerror(errno));
         return 1;
     }
--- sources/table/table.h
+++ sources/table/table.h
@@ -1,19 +1,13 @@
 /*
-  Filename: table.h
-
-  Made by: Alexis PETRILLO on 12/11/2017.
-
-**  Description: 
+**  Filename: table.h
+**
+**  Made by: Alexis PETRILLO on 12/11/2017.
+**
+**  Description: Header file for table
 */
 #ifndef YAMLDB_TABLE_H
 #define YAMLDB_TABLE_H
 
-#define RESOURCES_DIR "resources/"
-
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
 #include "../utils/xmalloc.h"
 
 int removeTable(const char *databaseName, const char *tableName);
