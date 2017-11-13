01d7ff52c0d021c9902398c562b93d39fa518372
Walkoss <walkoss@users.noreply.github.com>
2017-11-13 21:41:32 +0100

    Added feature to delete database (remove directory and its content)

--- sources/database/database.c
+++ sources/database/database.c
@@ -30,6 +30,48 @@ int addDatabase(const char *databaseName) {
     return 0;
 }
 
+int removeFile(const char *fpath,
+               const struct stat *sb,
+               int tflag,
+               struct FTW *ftwbuf) {
+    if (tflag == FTW_DP) {
+        if (rmdir(fpath) == -1) {
+            fprintf(stderr,
+                    "An error has occured when removing directory '%s': %s\n",
+                    fpath,
+                    strerror(errno));
+            return 1;
+        }
+    } else if (tflag == FTW_F) {
+        if (unlink(fpath) == -1) {
+            fprintf(stderr,
+                    "An error has occured when removing file '%s': %s\n",
+                    fpath,
+                    strerror(errno));
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int removeDatabase(const char *databaseName) {
+    char *path;
+
+    path = getDatabasePath(databaseName);
+    if (!path)
+        return 1;
+
+    if (nftw(path, removeFile, NOPENFD, FTW_DEPTH) == -1) {
+        fprintf(stderr, "An error has occured when removing database '%s': "
+                "%s\n", databaseName, strerror(errno));
+        free(path);
+        return 1;
+    }
+
+    free(path);
+    return 0;
+}
+
 /**
  * Based on RESOURCES_DIR, this function returns an absolute path to resources
  * Example: 'test' -> 'resources/test'
