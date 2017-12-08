02fd0fdcfafb3fc0e52da6f9253ad599db02916c
Alexis Petrillo <alexispetrillo@hotmail.fr>
2017-12-08 11:47:55 +0100

    fix some bugs

---
+++ sources/utils/strsplit.c
@@ -0,0 +1,7 @@
+/*
+**  Filename: strsplit.c
+**
+**  Made by: Alexis PETRILLO on 08/12/2017.
+**
+**  Description: 
+*/
---
+++ sources/utils/strsplit.h
@@ -0,0 +1,11 @@
+/*
+**  Filename: strsplit.h
+**
+**  Made by: Alexis PETRILLO on 08/12/2017.
+**
+**  Description: 
+*/
+#ifndef YAMLDB_STRSPLIT_H
+#define YAMLDB_STRSPLIT_H
+
+#endif //YAMLDB_STRSPLIT_H
